b6d84e15306be83dea76c8b617af8d169e4369cf
kangkang59812 <596286458@qq.com>
2019-05-28 21:28:45 +0800

    begin train with 5e-4 and 1e-3

--- train.py
+++ train.py
@@ -38,11 +38,16 @@ def main(args):
     # Build the models
     model = MIML(L=args.L, K=args.K, batch_size=args.batch_size,
                  fine_tune=args.fine_tune)
+    model = model.cuda()
+    model = nn.DataParallel(model, device_ids=[0, 1])
 
     optimizer = torch.optim.Adam(
-        [{'params': filter(lambda p: p.requires_grad, model.base_model.parameters()), 'lr': 5e-4},
-         {'params': model.sub_concept_layer.parameters(), 'lr': args.learning_rate}],
+        [{'params': filter(lambda p: p.requires_grad, model.module.base_model.parameters()), 'lr': 5e-4},
+         {'params': model.module.sub_concept_layer.parameters(), 'lr': args.learning_rate}],
     )
+    optimizer = nn.DataParallel(optimizer, device_ids=[0, 1])
+    critiation = nn.BCELoss()
+    critiation = nn.DataParallel(critiation, device_ids=[0, 1])
     time_start = time.time()
     total_step = len(train_loader)
     writer = SummaryWriter(log_dir='./log')
@@ -51,40 +56,27 @@ def main(args):
             images = imgs.cuda()
             targets = tars.float().cuda()
 
-    #         features = encoder(images)
-    #         outputs = decoder(features)
+            outputs = model(images)
+            loss = critiation(outputs, targets).mean()
+            
+            model.zero_grad()
+            loss.backward()
+            optimizer.module.step()
 
-    #         # begin with 10.69
-    #         pos = nn.functional.binary_cross_entropy(
-    #             outputs*targets, targets)*1
-    #         # begin with 0.7439
-    #         neg = nn.functional.binary_cross_entropy(
-    #             outputs*(1-targets)+targets, targets)*1
-
-    #         # begin with 11.4362
-    #         loss = (pos+neg)
-    #         decoder.zero_grad()
-    #         encoder.zero_grad()
-    #         loss.backward()
-    #         optimizer.step()
-
-    #         # Print log info
-    #         if i % args.log_step == 0:
-    #             time_end = time.time()
-    #             print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Time:{}'
-    #                   .format(epoch, args.num_epochs, i, total_step, loss.item(), time_end-time_start))
-    #             time_start = time_end
-    #         writer.add_scalars('three loss', {'loss': loss.item(
-    #         ), 'pos loss': pos.item(), 'neg loss': neg.item()}, i)
-    #         # if i == 10:
-    #         #     writer.close()
-    #     # Save the model checkpoints
-    #     if (epoch+1) % args.save_step == 0:
-    #         torch.save(decoder.state_dict(), os.path.join(
-    #             args.model_path, 'decoder-{}-{}.ckpt'.format(epoch+1, i+1)))
-    #         torch.save(encoder.state_dict(), os.path.join(
-    #             args.model_path, 'encoder-{}-{}.ckpt'.format(epoch+1, i+1)))
-    # writer.close()
+            # Print log info
+            if i % args.log_step == 0:
+                time_end = time.time()
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Time:{}'
+                      .format(epoch, args.num_epochs, i, total_step, loss.item(), time_end-time_start))
+                time_start = time_end
+            writer.add_scalars('loss', {'loss': loss.item()}, i)
+            # if i == 10:
+            #     writer.close()
+        # Save the model checkpoints
+        if (epoch+1) % args.save_step == 0:
+            torch.save(model.state_dict(), os.path.join(
+                args.model_path, 'decoder-{}-{}.ckpt'.format(epoch+1, i+1)))
+    writer.close()
 
 
 if __name__ == "__main__":
@@ -101,14 +93,14 @@ if __name__ == "__main__":
                         default='./img_tags.json', help='imgages id and tags')
     parser.add_argument('--caption_path', type=str, default='/home/lkk/datasets/coco2014/dataset_coco.json',
                         help='path for train annotation json file')
-    parser.add_argument('--log_step', type=int, default=100,
+    parser.add_argument('--log_step', type=int, default=1,
                         help='step size for prining log info')
     parser.add_argument('--save_step', type=int, default=1,
                         help='step size for saving trained models')
 
     # paraneters
     parser.add_argument('--num_epochs', type=int, default=5)
-    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--L', type=int, default=1024)
     parser.add_argument('--K', type=int, default=20)
     parser.add_argument('--fine_tune', action="store_true", default=True)
