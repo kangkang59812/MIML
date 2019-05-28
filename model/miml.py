d86496e1257b3abb0837ddab34b689f7d8fa3f98
kangkang59812 <596286458@qq.com>
2019-05-28 17:06:18 +0800

    before train

--- model/miml.py
+++ model/miml.py
@@ -2,13 +2,13 @@ import torch
 import torch.nn as nn
 import torchvision
 from torchsummary import summary
-
+from collections import OrderedDict
 device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
 
 
 class MIML(nn.Module):
 
-    def __init__(self, L=80, K=20, batch_size=8):
+    def __init__(self, L=1024, K=20, batch_size=8, fine_tune=True):
         """
         Arguments:
             L (int):
@@ -24,45 +24,71 @@ class MIML(nn.Module):
         base_model = torchvision.models.vgg16(pretrained=True)
         base_model = list(base_model.features)[:-1]
         self.base_model = nn.Sequential(*base_model)
-        self.conv1 = nn.Conv2d(512, 512, 1)
-        # (-1,512,14,14)
-        self.dropout1 = nn.Dropout(0.5)
+        self.fine_tune(fine_tune)
 
-        self.conv2 = nn.Conv2d(512, K*L, 1)
-        # input need reshape to (-1,L,K,H*W)
-        self.maxpool1 = nn.MaxPool2d((K, 1))
-        # reshape input to (-1,L,H*W)
-        # permute(0,2,1)
-        self.softmax1 = nn.Softmax(dim=2)
-        # permute(0,2,1)
-        # reshape to (-1,L,1,H*W)
-        self.maxpool2 = nn.MaxPool2d((1, 196))
-        # squeeze()
+        self.sub_concept_layer = nn.Sequential(OrderedDict([
+            ('conv1', nn.Conv2d(512, 512, 1)),
+            ('dropout1', nn.Dropout(0.5)),  # (-1,512,14,14)
+            ('conv2', nn.Conv2d(512, K*L, 1)),
+            # input need reshape to (-1,L,K,H*W)
+            ('maxpool1', nn.MaxPool2d((K, 1))),
+            # reshape input to (-1,L,H*W), # permute(0,2,1)
+            ('softmax1', nn.Softmax(dim=2)),
+            # permute(0,2,1) # reshape to (-1,L,1,H*W)
+            ('maxpool2', nn.MaxPool2d((1, 196)))
+        ]))
+        # self.conv1 = nn.Conv2d(512, 512, 1))
+
+        # self.dropout1=nn.Dropout(0.5)
+
+        # self.conv2=nn.Conv2d(512, K*L, 1)
+        # # input need reshape to (-1,L,K,H*W)
+        # self.maxpool1=nn.MaxPool2d((K, 1))
+        # # reshape input to (-1,L,H*W)
+        # # permute(0,2,1)
+        # self.softmax1=nn.Softmax(dim = 2)
+        # # permute(0,2,1)
+        # # reshape to (-1,L,1,H*W)
+        # self.maxpool2=nn.MaxPool2d((1, 196))
+        # # squeeze()
 
     def forward(self, x):
+        # IN:(8,3,224,224)-->OUT:(8,512,14,14)
         base_out = self.base_model(x)
+        # C,H,W = 512,14,14
         _, C, H, W = base_out.shape
+        # OUT:(8,512,14,14)
 
-        conv1_out = self.dropout1(self.conv1(base_out))
+        conv1_out = self.sub_concept_layer.dropout1(
+            self.sub_concept_layer.conv1(base_out))
 
         # shape -> (n_bags, (L * K), n_instances, 1)
-        conv2_out = self.conv2(conv1_out)
+        conv2_out = self.sub_concept_layer.conv2(conv1_out)
         # shape -> (n_bags, L, K, n_instances)
         conv2_out = conv2_out.reshape(-1, self.L, self.K, H*W)
         # shape -> (n_bags, L, 1, n_instances),remove dim: 1
-        maxpool1_out = self.maxpool1(conv2_out).squeeze()
+        maxpool1_out = self.sub_concept_layer.maxpool1(conv2_out).squeeze()
 
         # softmax
         permute1 = maxpool1_out.permute(0, 2, 1)
-        softmax1 = self.softmax1(permute1)
+        softmax1 = self.sub_concept_layer.softmax1(permute1)
         permute2 = softmax1.permute(0, 2, 1)
         # reshape = permute2.unsqueeze(2)
         reshape = permute2.reshape(-1, self.L, 1, H*W)
         # shape -> (n_bags, L, 1, 1)
-        maxpool2_out = self.maxpool2(reshape)
+        maxpool2_out = self.sub_concept_layer.maxpool2(reshape)
         out = maxpool2_out.squeeze()
 
         return out
+
+    def fine_tune(self, fine_tune=True):
+        # only fine_tune the last three convs
+        layer = -6
+        for p in self.base_model.parameters():
+            p.requires_grad = False
+        for c in list(self.base_model.children())[-6:]:
+            for p in c.parameters():
+                p.requires_grad = True
 
 
 if __name__ == "__main__":
---
+++ train.py
@@ -0,0 +1,118 @@
+import argparse
+import torch
+import torch.nn as nn
+import numpy as np
+import os
+from torch.nn.utils.rnn import pack_padded_sequence
+from torchvision import transforms
+import torch.nn.functional as F
+import time
+import random
+import json
+from utils.data_loader import get_loader
+from tensorboardX import SummaryWriter
+from model.miml import MIML
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+
+
+def visualization(features):
+    pass
+
+
+def main(args):
+    # Create model directory
+    if not os.path.exists(args.model_path):
+        os.makedirs(args.model_path)
+
+    print("load vocabulary ...")
+    # Load vocabulary wrapper
+    with open(args.vocab_path, 'r') as f:
+        vocab = json.load(f)
+
+    print("build data loader ...")
+    # load data
+    train_loader = get_loader(root=args.root, origin_file=args.caption_path, split=args.split,
+                              img_tags=args.img_tags, vocab=args.vocab_path, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+
+    print("build the models ...")
+    # Build the models
+    model = MIML(L=args.L, K=args.K, batch_size=args.batch_size,
+                 fine_tune=args.fine_tune)
+
+    optimizer = torch.optim.Adam(
+        [{'params': filter(lambda p: p.requires_grad, model.base_model.parameters()), 'lr': 5e-4},
+         {'params': model.sub_concept_layer.parameters(), 'lr': args.learning_rate}],
+    )
+    time_start = time.time()
+    total_step = len(train_loader)
+    writer = SummaryWriter(log_dir='./log')
+    for epoch in range(args.num_epochs):
+        for i, (imgs, tars, lens) in enumerate(train_loader):
+            images = imgs.cuda()
+            targets = tars.float().cuda()
+
+    #         features = encoder(images)
+    #         outputs = decoder(features)
+
+    #         # begin with 10.69
+    #         pos = nn.functional.binary_cross_entropy(
+    #             outputs*targets, targets)*1
+    #         # begin with 0.7439
+    #         neg = nn.functional.binary_cross_entropy(
+    #             outputs*(1-targets)+targets, targets)*1
+
+    #         # begin with 11.4362
+    #         loss = (pos+neg)
+    #         decoder.zero_grad()
+    #         encoder.zero_grad()
+    #         loss.backward()
+    #         optimizer.step()
+
+    #         # Print log info
+    #         if i % args.log_step == 0:
+    #             time_end = time.time()
+    #             print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Time:{}'
+    #                   .format(epoch, args.num_epochs, i, total_step, loss.item(), time_end-time_start))
+    #             time_start = time_end
+    #         writer.add_scalars('three loss', {'loss': loss.item(
+    #         ), 'pos loss': pos.item(), 'neg loss': neg.item()}, i)
+    #         # if i == 10:
+    #         #     writer.close()
+    #     # Save the model checkpoints
+    #     if (epoch+1) % args.save_step == 0:
+    #         torch.save(decoder.state_dict(), os.path.join(
+    #             args.model_path, 'decoder-{}-{}.ckpt'.format(epoch+1, i+1)))
+    #         torch.save(encoder.state_dict(), os.path.join(
+    #             args.model_path, 'encoder-{}-{}.ckpt'.format(epoch+1, i+1)))
+    # writer.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root', type=str,
+                        default='/home/lkk/datasets/coco2014', help='root path')
+    parser.add_argument('--model_path', type=str,
+                        default='models/', help='path for saving trained models')
+    parser.add_argument('--vocab_path', type=str,
+                        default='./vocab.json', help='path for vocabulary wrapper')
+    parser.add_argument('--split', type=str,
+                        default='train', help='train/val/test')
+    parser.add_argument('--img_tags', type=str,
+                        default='./img_tags.json', help='imgages id and tags')
+    parser.add_argument('--caption_path', type=str, default='/home/lkk/datasets/coco2014/dataset_coco.json',
+                        help='path for train annotation json file')
+    parser.add_argument('--log_step', type=int, default=100,
+                        help='step size for prining log info')
+    parser.add_argument('--save_step', type=int, default=1,
+                        help='step size for saving trained models')
+
+    # paraneters
+    parser.add_argument('--num_epochs', type=int, default=5)
+    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--L', type=int, default=1024)
+    parser.add_argument('--K', type=int, default=20)
+    parser.add_argument('--fine_tune', action="store_true", default=True)
+    parser.add_argument('--num_workers', type=int, default=0)
+    parser.add_argument('--learning_rate', type=float, default=1e-3)
+    args = parser.parse_args()
+    main(args)
