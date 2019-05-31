b97d511d1af7401f2cf722587dc4f9913dbb57da
kangkang59812 <596286458@qq.com>
2019-05-31 11:37:21 +0800

    adjustment the structure,add decay

--- train.py
+++ train.py
@@ -12,11 +12,13 @@ import json
 from utils.data_loader import get_loader
 from tensorboardX import SummaryWriter
 from model.miml import MIML
+from torch.optim import lr_scheduler
+from utils.utils import save_checkpoint, adjust_learning_rate
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
 
-def visualization(features):
-    pass
+# def visualization(features):
+#     pass
 
 
 def main(args):
@@ -31,53 +33,101 @@ def main(args):
 
     print("build data loader ...")
     # load data
-    train_loader = get_loader(root=args.root, origin_file=args.caption_path, split=args.split,
+    train_loader = get_loader(root=args.root, origin_file=args.caption_path, split='train',
                               img_tags=args.img_tags, vocab=args.vocab_path, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
-
+    val_loader = get_loader(root=args.root, origin_file=args.caption_path, split='val',
+                            img_tags=args.img_tags, vocab=args.vocab_path, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
     print("build the models ...")
     # Build the models
     model = MIML(L=args.L, K=args.K, batch_size=args.batch_size,
                  fine_tune=args.fine_tune)
     model = model.cuda()
     model = nn.DataParallel(model, device_ids=[0, 1])
-
     optimizer = torch.optim.Adam(
         [{'params': filter(lambda p: p.requires_grad, model.module.base_model.parameters()), 'lr': args.fine_tune_lr},
          {'params': model.module.sub_concept_layer.parameters(), 'lr': args.learning_rate}],
     )
-    optimizer = nn.DataParallel(optimizer, device_ids=[0, 1])
-    critiation = nn.BCELoss()
-    critiation = nn.DataParallel(critiation, device_ids=[0, 1])
-    time_start = time.time()
-    total_step = len(train_loader)
+
+    optimizer_SGD = torch.optim.SGD(
+        [{'params': filter(lambda p: p.requires_grad, model.module.base_model.parameters()), 'lr': args.fine_tune_lr},
+         {'params': model.module.sub_concept_layer.parameters(), 'lr': args.learning_rate}],
+    )
+
+    optimizer_Momentum = torch.optim.SGD(
+        [{'params': filter(lambda p: p.requires_grad, model.module.base_model.parameters()), 'lr': args.fine_tune_lr},
+         {'params': model.module.sub_concept_layer.parameters(), 'lr': args.learning_rate}],
+        momentum=0.8
+    )
+
+    optimizer_RMSprop = torch.optim.RMSprop(
+        [{'params': filter(lambda p: p.requires_grad, model.module.base_model.parameters()), 'lr': args.fine_tune_lr},
+         {'params': model.module.sub_concept_layer.parameters(), 'lr': args.learning_rate}],
+        alpha=0.9
+    )
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.8)
+    # optimizer = nn.DataParallel(optimizer, device_ids=[0, 1])
+    criterion = nn.BCELoss()
+    # criterion = nn.DataParallel(criterion, device_ids=[0, 1])
+    best_ac = 0
+    epochs_since_improvement = 0
     writer = SummaryWriter(log_dir='./log')
     for epoch in range(args.num_epochs):
-        for i, (imgs, tars, lens) in enumerate(train_loader):
-            images = imgs.cuda()
-            targets = tars.float().cuda()
 
-            outputs = model(images)
-            loss = critiation(outputs, targets).mean()
+        # Decay learning rate if there is no improvement for 8 consecutive epochs, and terminate training after 20
+        # if epochs_since_improvement == 20:
+        #     break
 
-            model.zero_grad()
-            loss.backward()
-            optimizer.module.step()
+        if epochs_since_improvement > 0 and epochs_since_improvement % 2 == 0:
+            # scheduler.step()
+            adjust_learning_rate(optimizer, 0.8)
 
-            # Print log info
-            if i % args.log_step == 0:
-                time_end = time.time()
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Time:{}'
-                      .format(epoch, args.num_epochs, i, total_step, loss.item(), time_end-time_start))
-                time_start = time_end
-            writer.add_scalars(
-                'loss', {'loss': loss.item()}, epoch*total_step+i)
-            # if i == 10:
-            #     writer.close()
-        # Save the model checkpoints
-        if (epoch+1) % args.save_step == 0:
-            torch.save(model.state_dict(), os.path.join(
-                args.model_path, 'model-{}-{}.ckpt'.format(epoch+1, i+1)))
+        train(args, train_loader=train_loader, model=model, criterion=criterion,
+              optimizer=optimizer, epoch=epoch, writer=writer)
+
+        accuracy = validate()
+
+        is_best = accuracy > best_ac
+        best_ac = max(accuracy, best_ac)
+        if not is_best:
+            epochs_since_improvement += 1
+        else:
+            epochs_since_improvement = 0
+
+        save model
+        save_checkpoint(data_name='model', epoch=epoch, epochs_since_improvement=epochs_since_improvement, model=model,
+                        optimizer=optimizer, accuracy=accuracy, is_best=is_best)
     writer.close()
+
+
+def train(args, train_loader, model, criterion, optimizer, epoch, writer):
+    model.train()
+    total_step = len(train_loader)
+    time_start = time.time()
+    for i, (imgs, tars, lens) in enumerate(train_loader):
+        images = imgs.cuda()
+        targets = tars.float().cuda()
+
+        outputs = model(images)
+        loss = criterion(outputs, targets)  # .mean()
+
+        model.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # optimizer.module.step()
+        # Print log info
+        if i % args.log_step == 0:
+            time_end = time.time()
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Time:{}'
+                  .format(epoch, args.num_epochs, i, total_step, loss.item(), time_end-time_start))
+            time_start = time_end
+        writer.add_scalars(
+            'loss', {'loss': loss.item()}, epoch*total_step+i)
+
+
+def validate():
+    accuracy = 0
+    return accuracy
 
 
 if __name__ == "__main__":
@@ -88,8 +138,6 @@ if __name__ == "__main__":
                         default='models/', help='path for saving trained models')
     parser.add_argument('--vocab_path', type=str,
                         default='./vocab.json', help='path for vocabulary wrapper')
-    parser.add_argument('--split', type=str,
-                        default='train', help='train/val/test')
     parser.add_argument('--img_tags', type=str,
                         default='./img_tags.json', help='imgages id and tags')
     parser.add_argument('--caption_path', type=str, default='/home/lkk/datasets/coco2014/dataset_coco.json',
@@ -100,13 +148,13 @@ if __name__ == "__main__":
                         help='step size for saving trained models')
 
     # paraneters
-    parser.add_argument('--num_epochs', type=int, default=5)
+    parser.add_argument('--num_epochs', type=int, default=10)
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--L', type=int, default=1024)
     parser.add_argument('--K', type=int, default=20)
     parser.add_argument('--fine_tune', action="store_true", default=True)
     parser.add_argument('--num_workers', type=int, default=0)
-    parser.add_argument('--fine_tune_lr', type=float, default=4e-5)
-    parser.add_argument('--learning_rate', type=float, default=4e-4)
+    parser.add_argument('--fine_tune_lr', type=float, default=1e-4)
+    parser.add_argument('--learning_rate', type=float, default=2e-3)
     args = parser.parse_args()
     main(args)
